d2f412b7aa6d2cbafc04a76897160280ba349c93
nandan10 <kumari.singh@gmail.com>
2023-09-20 16:49:57 +0530

    Revised

--- app/src/main/java/com/navigation/smartcane/EmergencyCall.java
+++ app/src/main/java/com/navigation/smartcane/EmergencyCall.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.PermissionChecker;
@@ -17,27 +18,31 @@ public class EmergencyCall extends AppCompatActivity {
     private static final int IGNORE_BATTERY_OPTIMIZATION_REQUEST = 1002;
     private static final int PICK_CONTACT = 1;
     private PermissionChecker PackageManager;
+    TextView Called;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.call_activity);
+        Called = findViewById(R.id.Call);
         DbHelper db = new DbHelper(this);
         List<ContactModel> list = db.getAllContacts();
+
 
         // send SMS to each contact
         for (ContactModel c : list) {
             String phone_number = c.getPhoneNo();
+            String Name =  c.getName();
 
             // Getting instance of Intent with action as ACTION_CALL
             Intent phone_intent = new Intent(Intent.ACTION_CALL);
 
             // Set data of Intent through Uri by parsing phone number
             phone_intent.setData(Uri.parse("tel:" + phone_number));
-
+            Called.setText(Name+"\n"+phone_number);
             // start Intent
             startActivity(phone_intent);
-        }}
+        } }
     public void goBackPressed(View view) {
         onBackPressed();
     }}
--- app/src/main/java/com/navigation/smartcane/EmergencySms.java
+++ app/src/main/java/com/navigation/smartcane/EmergencySms.java
@@ -18,6 +18,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -39,12 +40,25 @@ public class EmergencySms extends AppCompatActivity {
     List<ContactModel> list;
     CustomAdapter customAdapter;
     private String phoneNo;
+    ListView sms;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sms_activity);
+        sms = findViewById(R.id.listView1);
+        DbHelper db = new DbHelper(this);
+        List<ContactModel> list = db.getAllContacts();
+        CustomAdapter customAdapter = new CustomAdapter(this, list);
+        customAdapter.addAll();
+       /* for (ContactModel c : list) {
+            String phone_number = c.getPhoneNo();
+            String Name =  c.getName();
+        Message.setText(Name+"\n"+phone_number);}*/
+        sms.setAdapter(customAdapter);
+//Message.setText();
+
 
         // check for runtime permissions
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -76,10 +90,10 @@ public class EmergencySms extends AppCompatActivity {
         }
 
 
-       // button1 = findViewById(R.id.Button1);
-      //  listView = (ListView) findViewById(R.id.ListView);
-       // db = new DbHelper(this);
-       // list = db.getAllContacts();
+        // button1 = findViewById(R.id.Button1);
+        //  listView = (ListView) findViewById(R.id.ListView);
+        // db = new DbHelper(this);
+        // list = db.getAllContacts();
        /* customAdapter = new CustomAdapter(this, list);
        // listView.setAdapter(customAdapter);
 
@@ -135,6 +149,7 @@ public class EmergencySms extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+
         // get the contact from the PhoneBook of device
         switch (requestCode) {
             case (PICK_CONTACT):
@@ -147,21 +162,29 @@ public class EmergencySms extends AppCompatActivity {
                         String id = c.getString(c.getColumnIndexOrThrow(ContactsContract.Contacts._ID));
                         @SuppressLint("Range") String hasPhone = c.getString(c.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
                         String phone = null;
+
                         try {
                             if (hasPhone.equalsIgnoreCase("1")) {
                                 Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + id, null, null);
                                 phones.moveToFirst();
                                 phone = phones.getString(phones.getColumnIndex("data1"));
+
                             }
                             String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
                             db.addcontact(new ContactModel(0, name, phone));
+
+
                             list = db.getAllContacts();
                             customAdapter.refresh(list);
+                            // Message.setText(db.count());
+
+
                         } catch (Exception ex) {
                         }
                     }
                 }
                 break;
+
         }
     }
 
